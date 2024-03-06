2a8d3097bc4362d6d82d724611a056f2975e0922
Nomee07 <gigabanomfundo@gmail.com>
2024-03-06 05:15:40 -0800

    Api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,23 +7,18 @@ If an invalid subreddit is given, the function should return 0
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     Function that queries the Reddit API
     - If not a valid subreddit, return 0.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom"}
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
 
-    try:
-        req = requests.get(url, headers=headers)
-        req.raise_for_status()  # Raise an error for unsuccessful status codes
-        data = req.json().get("data")
-        subscribers = data.get("subscribers")
-        return subscribers
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return 0
-    except KeyError as e:
-        print(f"Error: Key not found in JSON response - {e}")
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
         return 0
